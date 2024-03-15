d01b26b2fdb202e445eb145bcb428a98b421f3f8
Anqiiiiiiiii <122634853+Anqiiiiiiiii@users.noreply.github.com>
2024-03-15 15:31:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,79 @@
+maping_sch = {
+    "chemistry_lab":{
+        "description":["The lab is brightly lit with white walls and metal countertops. Glass bottles, test tubes and instruments are neatly arranged and chemical reagent bottles are clearly"], 
+        "tool":["sufuric acid"]
+    },
+    "physic_lab":{
+        "description":[" In the physics laboratory, reflected in the neat lab bench. The equipment is complete, from high precision instruments to various circuit components. A faint chemical odor permeates the lab, and a slight mechanical hum and electrical cacophony form the background sound."],
+        "tool":["ball"],
+    },
+    "biology_lab":{
+        "description":["Microscopes, test tubes and petri dishes are arranged on long tables. Lab benches were neat and organized, with chemicals and biological samples properly stored. The fresh scent of disinfectant filled the air. "],
+        "tool":["None"]
+    },
+    "math_classroom":{
+        "description":[" In the math classroom, the whiteboard is covered with math formulas and diagrams. The worn tables were piled up in a small mountain."],
+        "tool":["calculator"]
+    },
+    "english_classroom":{
+        "description":["The English classroom wall lined with shelves filled with books ranging from classic literature to contemporary novel."],
+        "tool":["bible"]
+    },
+    "washroom":{
+        "description":["Abandoned toilets with splintered walls, rusty facets, and cracked sinks. The broken mirror reflects a blurred figure. Moss grows in the cracks of the bricks on the floor, filling the room with a gloomy odor"],
+      "tool":["None"]
+    },
+    "history_classroom":{
+        "description":[" In the dilapidated history classroom, the walls peeled back and dim light poured through the broken windows. Old desks and chairs are scattered. Some worn literature was spread out on the table."],
+        "tool":["science information"]
+    },
+    "psychological_classroom":{
+        "description":["In the dilapidated psych classroom, the walls were peeling away, and dim light spilled through the cracked windows, creating an eerie glow. The tables and chairs in the center of the classroom were messy and collapsed, scattered with broken pens and papers."],
+        "tool":["None"]
+    },
+    "computer_science":{
+        "description":["The computer lab is dim, shrouded in dust and silence. Old desks and chairs are scattered and tilted, the walls are peeling, and exposed wires hang in a jumble."],
+        "tool":["usb"]
+    },
+    "gym":{
+        "description":["In the dilapidated stadium, walls are peeling and exposed rebar is everywhere. Dust and cobwebs intertwine, shrouding the abandoned bleachers."],
+        "tool":["ball"]
+    },
+    "cafeteria":{
+        "description":[" In the dilapidated cafeteria, worn tables and chairs are set up, their surfaces covered in dust and stains. The walls peeled away, revealing patches of concrete, and garbage piled up in the corners. Dim lights cast shadows through tattered lampshades. "],
+        "tool":["fork"]
+    }
+}
+
+map = [
+  ["chem", "math", "his", "gym"],
+  ["lab, english, psyc,front"],
+  ["bio", "wc", "cs", "cafe"]]
+
+
+position = {
+    "move":["up, down, right, left"]
+}
+
+bagpack = {
+    "player_tools":[""],
+}
+
+player_positions = {
+    row:[0], 
+    col:[0]
+}
+
+def player_movement():
+    movement = input("Where do you want to go?")
+    if movement.lower() == "up":
+        player_position = map[player_positions[row + 1]][player_positions[col + 1]]
+    elif movement.lower() == "down":
+        pass
+    elif movement.lower() == "right":
+        pass
+    elif movement.lower() == "left":
+        pass
+    else:
+        print("Wrong input~ please make sure enter the opition exist in the menu.")
+      
